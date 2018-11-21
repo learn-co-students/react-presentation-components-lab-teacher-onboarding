5ed829f9586f7242f6bf80068972abf49a1117b8
Edward Sparks <edd_sparks@hotmail.com>
2018-11-21 08:03:30 +0000

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,12 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react'
+
+export default class SimpleComponent extends Component {
+  state ={
+    mood: 'happy'
+  }
+
+  render() {
+    return (<div>{this.state.mood}</div>)
+  }
+}
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,6 @@
-// Code SimplerComponent Here
+import React from 'react'
+
+const SimplerComponent = ({ handleClick }) =>
+  <button onClick={ handleClick }>I am just happy</button>
+
+export default SimplerComponent
